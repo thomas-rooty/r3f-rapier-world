c95a3c6cfa09d4386cea08e6915f45b562dd78de
Thomas Caron <tc.caron91@gmail.com>
2023-09-21 15:14:38 +0200

    More comprehensive order

--- src/components/Experience.tsx
+++ src/components/Experience.tsx
@@ -9,11 +9,11 @@ const Experience = () => {
       <Sky sunPosition={[100, 20, 100]} />
       <ambientLight intensity={1} />
       <pointLight castShadow intensity={0.8} position={[100, 100, 100]} />
+      <PointerLockControls />
       <Physics debug gravity={[0, -10, 0]}>
         <Player />
         <Ground />
       </Physics>
-      <PointerLockControls />
     </>
   )
 }
